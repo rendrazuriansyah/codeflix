8e9635bf597b81c485178a11a585b5b7f2d05102
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-03-24 10:07:38 +0700

    feat(models): add relationships to Membership and Plan
    Adds `plan` and `user` relationships to the `Membership` model.
    Adds `memberships` and `users` relationships to the `Plan` model.
    This allows for querying related data more efficiently.

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Membership extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'start_date',
+        'end_date',
+        'active',
+    ];
+
+    public function plan() 
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'price',
+        'duration',
+        'resolution',
+        'max_devices'
+    ];
+
+    public function memberships() 
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'memberships', 'plan_id', 'user_id');
+    }
 }
