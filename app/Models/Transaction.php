9e2b368f43f34a82b4a65ac60df272305aeb82f0
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-04-04 16:32:01 +0700

    feat(transactions): add transaction support
    - Adds Transaction model with attributes for transaction details
    - Creates database migration for transactions table
    - Integrates 'Midtrans PHP' library for payment processing

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\Plan;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'transaction_number',
+        'total_amount',
+        'payment_status',
+        'midtrans_snap_token',
+        'midtrans_booking_code',
+        'midtrans_transaction_code',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
--- database/migrations/2025_04_04_091423_create_transactions_table.php
+++ database/migrations/2025_04_04_091423_create_transactions_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('plan_id')->constrained();
+            $table->string('transaction_number')->unique();
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('payment_status', ['pending', 'success', 'failed', 'expired']);
+            $table->string('midtrans_snap_token')->nullable();
+            $table->string('midtrans_booking_code')->nullable();
+            $table->string('midtrans_transaction_code')->nullable();
             $table->timestamps();
         });
     }
